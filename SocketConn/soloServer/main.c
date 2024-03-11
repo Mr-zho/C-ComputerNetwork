4474667ed86b51b4509c662c6dd1038102d66a42
wenliang <wenliang@163.com>
2024-03-11 09:28:46 +0800

    [修改] 设置端口复用

--- SocketConn/soloServer/main.c
+++ SocketConn/soloServer/main.c
@@ -5,6 +5,7 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <unistd.h>
 
 #define SERVER_PORT 8080
 
@@ -19,6 +20,15 @@ int main()
     printf("sockfd is %d\n", sockfd);
 
 
+    /* 设置端口复用 */
+    int optVal = 1;
+    int ret = setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (const void *)&optVal, sizeof(optVal));
+    if (ret == -1)
+    {
+        perror("setsockopt error");
+        exit(-1);
+    }
+
     struct sockaddr_in localAddress;
     /* 清除脏数据 */
     memset(&localAddress, 0, sizeof(localAddress));
@@ -28,7 +38,7 @@ int main()
 
     socklen_t localAddressLength = sizeof(localAddress);
     /* 绑定端口IP. */
-    int ret = bind(sockfd, (const struct sockaddr *)&localAddress, localAddressLength);
+    ret = bind(sockfd, (const struct sockaddr *)&localAddress, localAddressLength);
     if (ret == -1)
     {
         perror("bind error");
@@ -46,6 +56,8 @@ int main()
     struct sockaddr_in clientAddress;
     memset(&clientAddress, 0, sizeof(clientAddress));
     socklen_t clientAddressLength = 0;
+    
+    /* accept的返回值是和【客户端通信】的文件描述符 */
     int connfd = accept(sockfd, (struct sockaddr *)&clientAddress, &clientAddressLength);
     if (connfd == -1)
     {
@@ -53,5 +65,15 @@ int main()
         exit(-1);
     }
     printf("connfd is %d\n", connfd);
+
+    /* */
+    char buffer[32] = { 0 };
+    strcpy(buffer, "偶像");
+    write(connfd, buffer, sizeof(buffer) - 1);
+
+
+    /* 回收资源 */
+    close(connfd);
+    close(sockfd);
     return 0;
 }
