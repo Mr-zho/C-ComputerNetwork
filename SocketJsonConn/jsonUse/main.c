9d84c6c6117ba6cc9a19efb09554e60598e8e298
wenliang <wenliang@163.com>
2024-03-11 15:57:42 +0800

    [新增] json数组的使用

--- SocketJsonConn/jsonUse/main.c
+++ SocketJsonConn/jsonUse/main.c
@@ -3,7 +3,8 @@
 
 
 /* 
-    需求: 将{ "type": 2, "name":"zhou", "passwd" : 123 }
+    需求: 将
+    { "type": 2, "name":"zhou", "passwd" : 123 , "books" : ["CPP", "JAVA", "PHP"] }
 */
 
 /* json是<key, value> */
@@ -21,6 +22,12 @@ int main()
     json_object_object_add(object, "type", json_object_new_int64(1));
     json_object_object_add(object, "name", json_object_new_string("zhou"));
     json_object_object_add(object, "passwd", json_object_new_int64(123));
+
+    struct json_object * arrayBooks = json_object_new_array();
+    json_object_array_add(arrayBooks, json_object_new_string("C++"));
+    json_object_array_add(arrayBooks, json_object_new_string("JAVA"));
+    json_object_array_add(arrayBooks, json_object_new_string("PHP"));
+    json_object_object_add(object, "books", arrayBooks);
     
     /* 将json对象转化成字符串 */
     const char * str = json_object_to_json_string(object);
@@ -35,5 +42,13 @@ int main()
     printf("name:%s\n", json_object_get_string(json_object_object_get(obj, "name")));
     printf("passwd:%ld\n", json_object_get_int64(json_object_object_get(obj, "passwd")));
 
+    struct json_object * bookLists = json_object_object_get(obj, "books");
+    /* 数组有多长 */
+    size_t size = json_object_array_length(bookLists);
+    printf("size:%ld\n", size);
+    for (int idx = 0; idx < size; idx++)
+    {
+        printf("book[%d] = %s\n", idx, json_object_get_string(json_object_array_get_idx(bookLists, idx)));
+    }
     return 0;
 }
